3c644f1460240bbd87697276640cfbeab6e8b37f
beeravellisaisree <85607925+beeravellisaisree@users.noreply.github.com>
2021-06-10 17:31:22 +0530

    files.updated

---
+++ the magic.py
@@ -0,0 +1,14 @@
+print("its a magic")
+print(".\n.\n.\n.\n.\n.\n")
+print("wondering where")
+print(" minimise the present screen then you can see magic in python turtle graphics window ")
+import turtle as turtle
+my_pen = turtle.Turtle()
+for i in range (50):
+    my_pen.forward(50)
+    my_pen.right(144)
+turtle.done()    
+
+
+
+
